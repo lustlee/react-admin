28b2231a41274a77f283f2645bfe12999f27427a
Ramazan <r.tokoshev@gmail.com>
2023-07-19 20:31:59 +0600

    feat: styled the footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import './footer.scss'
 
 const Footer = () => {
-  return <div className="footer">footer</div>
+  return (
+    <div className="footer">
+      <span>aramushiadmin</span>
+      <span>© Aramushi Dev Admin Dashboard</span>
+    </div>
+  )
 }
 
 export default Footer
