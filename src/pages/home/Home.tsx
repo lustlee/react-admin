57a52ae94386578bcbbbadede1b6d0e05435bb7a
Ramazan <r.tokoshev@gmail.com>
2023-07-20 16:29:16 +0600

    feat: I added a new component for drawing charts, also added fake data for the necessary display of these charts, made styling and made props

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,7 @@
 import './home.scss'
 import TopBox from "../../components/topBox/TopBox.tsx";
+import ChartBox from "../../components/chartBox/chartBox.tsx";
+import {chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser} from "../../data.ts";
 
 const Home = () => {
     return (
@@ -7,11 +9,11 @@ const Home = () => {
             <div className="box box1">
                 <TopBox/>
             </div>
-            <div className="box box2">box2</div>
-            <div className="box box3">box3</div>
-            <div className="box box4">box4</div>
-            <div className="box box5">box5</div>
-            <div className="box box6">box6</div>
+            <div className="box box2"><ChartBox {...chartBoxUser}/></div>
+            <div className="box box3"><ChartBox {...chartBoxProduct}/></div>
+            <div className="box box4"></div>
+            <div className="box box5"><ChartBox {...chartBoxConversion}/></div>
+            <div className="box box6"><ChartBox {...chartBoxRevenue}/></div>
             <div className="box box7">box7</div>
             <div className="box box8">box8</div>
             <div className="box box9">box9</div>
