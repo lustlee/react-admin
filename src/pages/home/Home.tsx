5c650beb9df78ad7eda9078548ba6b95cc999abd
Ramazan <r.tokoshev@gmail.com>
2023-07-20 18:15:06 +0600

    feat: made components with a graph and data output on the client

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,17 @@
 import './home.scss'
 import TopBox from "../../components/topBox/TopBox.tsx";
 import ChartBox from "../../components/chartBox/chartBox.tsx";
-import {chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser} from "../../data.ts";
+import {
+    barChartBoxRevenue,
+    barChartBoxVisit,
+    chartBoxConversion,
+    chartBoxProduct,
+    chartBoxRevenue,
+    chartBoxUser
+} from "../../data.ts";
+import BarChartBox from "../../components/barChartBox/barChartBox.tsx";
+import PieChartBox from "../../components/pieChartBox/PieChartBox.tsx";
+import BigChartBox from "../../components/bigChartBox/BigChartBox.tsx";
 
 const Home = () => {
     return (
@@ -11,12 +21,12 @@ const Home = () => {
             </div>
             <div className="box box2"><ChartBox {...chartBoxUser}/></div>
             <div className="box box3"><ChartBox {...chartBoxProduct}/></div>
-            <div className="box box4"></div>
+            <div className="box box4"><PieChartBox /></div>
             <div className="box box5"><ChartBox {...chartBoxConversion}/></div>
             <div className="box box6"><ChartBox {...chartBoxRevenue}/></div>
-            <div className="box box7">box7</div>
-            <div className="box box8">box8</div>
-            <div className="box box9">box9</div>
+            <div className="box box7"><BigChartBox /></div>
+            <div className="box box8"><BarChartBox {...barChartBoxVisit} /></div>
+            <div className="box box9"><BarChartBox {...barChartBoxRevenue} /></div>
         </div>
     )
 }
